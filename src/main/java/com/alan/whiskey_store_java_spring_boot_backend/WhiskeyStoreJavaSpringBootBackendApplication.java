3a1ada87e61f4f69c7e73331b8123b3ac2915c6c
Alan <140322633+arthenux@users.noreply.github.com>
2026-03-27 17:08:52 +0000

    Initial commit

---
+++ src/main/java/com/alan/whiskey_store_java_spring_boot_backend/WhiskeyStoreJavaSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alan.whiskey_store_java_spring_boot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskeyStoreJavaSpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskeyStoreJavaSpringBootBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alan/whiskey_store_java_spring_boot_backend/WhiskeyStoreJavaSpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alan.whiskey_store_java_spring_boot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskeyStoreJavaSpringBootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
